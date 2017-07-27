b1447df2fac87dd3cfc0db77550e1d4cece92780
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2017-07-27 10:03:32 +0700

    Merge with last project

--- gen/com/gerejadibandung/minuteswiththelord/Manifest.java
+++
@@ -1,7 +0,0 @@
-/*___Generated_by_IDEA___*/
-
-package com.gerejadibandung.minuteswiththelord;
-
-/* This stub is only used by the IDE. It is NOT the Manifest class actually packed into the APK */
-public final class Manifest {
-}
